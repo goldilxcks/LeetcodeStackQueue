1084957776ab7482d1a4805d878cfd13bc427c7d
Zlata Leskiv <zlataleskivofficial@gmail.com>
2026-04-06 14:30:41 +0300

    Added maximum frequcny stack task with photo

--- maximum_frequency_stack/solution.py
+++ maximum_frequency_stack/solution.py
@@ -9,23 +9,27 @@ class Stack:
     """Stack class"""
     def __init__(self):
         self.head = None
+
     def push(self, item):
-        """push docstring"""
-        self.head = Node(item, next_node=self.head)
+        """Push item to top"""
+        self.head = Node(item, self.head)
+
     def pop(self):
-        """pop docstring"""
+        """Remove top item"""
         if self.head is None:
             raise IndexError
-        dele = self.head.val
+        value = self.head.val
         self.head = self.head.next
-        return dele
+        return value
+
     def peek(self):
-        """peek docstring"""
+        """Return top item"""
         if self.head is None:
             raise IndexError
         return self.head.val
+
     def is_empty(self):
-        """is_empty docstring"""
+        """Check if stack is empty"""
         return self.head is None
 
 class ValueFreqNode:
@@ -42,65 +46,76 @@ class FreqGroupNode:
         self.stack = Stack()
         self.next = next_node
 
+
 class FreqStack:
     """Frequency Stack class"""
     def __init__(self):
-        self.values_head = None
-        self.groups_head = None
+        self.val_head = None
+        self.grp_head = None
         self.max_freq = 0
+
     def push(self, val: int) -> None:
-        """pushes an integer val onto the top of the stack."""
-        curr_1 = self.values_head
-        value_node = None
-        while curr_1:
-            if curr_1.val == val:
-                value_node = curr_1
+        """Pushes an integer val onto the top of the stack."""
+        val_node = self.val_head
+        while val_node is not None:
+            if val_node.val == val:
                 break
-            curr_1 = curr_1.next
-        if value_node is None:
-            value_node = ValueFreqNode(val, 0, self.values_head)
-            self.values_head = value_node
-        value_node.freq += 1
-        new_freq = value_node.freq
-        curr_2 = self.groups_head
-        group_node = None
-        while curr_2:
-            if curr_2.freq == new_freq:
-                group_node = curr_2
-                break
-            curr_2 = curr_2.next
-        if group_node is None:
-            group_node = FreqGroupNode(new_freq, self.groups_head)
-            self.groups_head = group_node
-        group_node.stack.push(val)
-        if new_freq > self.max_freq:
-            self.max_freq = new_freq
+            val_node = val_node.next
+        if val_node is None:
+            self.val_head = ValueFreqNode(val, 1, self.val_head)
+            val_node = self.val_head
+        else:
+            val_node.freq += 1
+        frq = val_node.freq
+        grp_node = self.grp_head
+        if grp_node is None or grp_node.freq == frq:
+            if grp_node is None:
+                self.grp_head = FreqGroupNode(frq)
+                grp_node = self.grp_head
+            else:
+                grp_node.stack.push(val)
+                if frq > self.max_freq:
+                    self.max_freq = frq
+                return
+        else:
+            while grp_node.next is not None and grp_node.next.freq != frq:
+                grp_node = grp_node.next
+            if grp_node.next is None:
+                grp_node.next = FreqGroupNode(frq)
+            grp_node = grp_node.next
+        grp_node.stack.push(val)
+        if frq > self.max_freq:
+            self.max_freq = frq
 
     def pop(self) -> int:
-        """removes and returns the most frequent element in the stack."""
-        curr = self.groups_head
-        last = None
-        group = None
-        while curr:
-            if curr.freq == self.max_freq:
-                group = curr
+        """Removes and returns the most frequent element in the stack."""
+        grp_node = self.grp_head
+        if grp_node is None:
+            raise IndexError
+        if grp_node.freq == self.max_freq:
+            value = grp_node.stack.pop()
+            if grp_node.stack.is_empty():
+                self.grp_head = grp_node.next
+                self.max_freq -= 1
+        else:
+            while grp_node.next is not None and grp_node.next.freq != self.max_freq:
+                grp_node = grp_node.next
+            target_group = grp_node.next
+            value = target_group.stack.pop()
+            if target_group.stack.is_empty():
+                grp_node.next = target_group.next
+                self.max_freq -= 1
+        val_node = self.val_head
+        while val_node is not None:
+            if val_node.val == value:
+                val_node.freq -= 1
                 break
-            last = curr
-            curr = curr.next
-        first = group.stack.pop()
-        current = self.values_head
-        while current:
-            if current.val == first:
-                current.freq -= 1
-                break
-            current = current.next
-        if group.stack.is_empty():
-            if last is None:
-                self.groups_head = group.next
-            else:
-                last.next = group.next
-            self.max_freq -= 1
-        return first
+            val_node = val_node.next
+        return value
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
 # obj.push(val)
